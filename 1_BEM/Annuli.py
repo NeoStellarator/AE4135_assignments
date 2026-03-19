ff1440e0a7b44fa5f74187f781e5abbc41f9e454
Kasper Weel <kasper.weel@to70.eu>
2026-03-19 21:55:36 +0100

    added thrust and torque calculation

--- 1_BEM/Annuli.py
+++ 1_BEM/Annuli.py
@@ -26,6 +26,7 @@ class Annuli:
                  B:float,
                  c_R:float,
                  beta:float,
+                 rho:float,
                  a0:float=0,
                  aline0:float=0,
                  isPropeller:bool = False):
@@ -45,7 +46,7 @@ class Annuli:
         self.chord = c_R*R
         self.A = np.pi*((r2_R *R )**2- (r1_R*R)**2)
         self.isPropeller = isPropeller
-
+        self.rho=rho
         # iteration initialization
         self.a0     = a0
         self.aline0 = aline0
@@ -53,6 +54,7 @@ class Annuli:
         self.aline_hist = []
         # read & store polar data
         self.polar_data=self._load_polar_data(polar_path)
+      
       
         # perform the iteration
         self.run_iteration()
@@ -184,25 +186,8 @@ class Annuli:
 
             # update iteration count
             i += 1
-            if not res.converged:
-                print("not converged!!")
 
-        # recompute and store the final converged state
-        self.calculate_residual(self.phi)
-        self.alpha = self.hist["alpha"][-1]
-        self.Cl = self.hist["Cl"][-1]
-        self.Cd = self.hist["Cd"][-1]
-        self.Cx = self.hist["Cx"][-1]
-        self.Cy = self.hist["Cy"][-1]
-        self.F  = self.hist["F"][-1]
-        self.CT = self.hist["CT"][-1]
-        self.CP = self.hist["CP"][-1]
-        self.Ca = self.hist["Ca"][-1]
-        self.CQ = self.hist["CQ"][-1]
-        self.a = self.hist["a"][-1]
-        self.aline = self.hist["aline"][-1]
-
-        # print(i)
+        print(i)
         # save converged results
         self.phi = np.rad2deg(phi)
         self.alpha = alpha_deg
@@ -216,6 +201,13 @@ class Annuli:
         self.aline = aline
         self.Ux = Ux
         self.Uy= Uy
+    def calculate_forces(self):
+        Ux = self.Uinf*(1+self.a)
+        Uy = (1-self.aline)*self.Omega*self.r_R*self.R
+        w2 = Ux**2+Uy**2
+        thrust = 0.5 * self.Cx * self.rho * w2 * self.chord * (self.r2_R-self.r1_R)*self.R * self.B
+        torque = 0.5 * self.Cy * self.rho * w2 * self.chord * (self.r2_R-self.r1_R)*self.R * self.B * self.r_R*self.R
+        return [thrust, torque]
 
 
 if __name__ == "__main__":
