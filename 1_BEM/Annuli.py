72e957f192842c56ff73a1122f01fafa97d94f6e
Alfonso Medina Marrero <alfitrueno@gmail.com>
2026-03-21 17:21:22 +0100

    Update annuli to: - work with turbine airfoils - axial induction with correction for heaviliy loaded turbine operation - updated storage

--- 1_BEM/Annuli.py
+++ 1_BEM/Annuli.py
@@ -15,7 +15,8 @@ class Annuli:
                  beta:float,
                  B:int, 
                  J:float,
-                 r_R_H:float=0):
+                 r_R_H:float=0,
+                 is_prop:bool=True):
 
         # save blade 
         self.r_R   = r_R   # blade radial coordinate
@@ -27,10 +28,13 @@ class Annuli:
         self.TSR   = np.pi/J # tip speed ratio
         self.sig   = self.B/(2*np.pi)*self.c_R/self.r_R # blade solidity
 
+        self.is_prop = is_prop
+        
         # read & store polar data
         self.polar_data=self._load_polar_data(polar_path)
 
-        self.phi = self.solve()
+        # solve annuli
+        self.solve()
 
 
     def _load_polar_data(self, polar_path:Path|str) -> Dict[str, np.ndarray]:
@@ -46,12 +50,37 @@ class Annuli:
     
     def calculate_Cl(self, alpha:np.ndarray|float) -> np.ndarray|float:
         """Method to compute Cl at a given angle of attack"""
-        return np.interp(alpha,self.polar_data["alpha"],self.polar_data["Cl"])
+        if self.is_prop:
+            return np.interp(alpha, 
+                             self.polar_data["alpha"], 
+                             self.polar_data["Cl"])
+        else:
+            return -np.interp(-alpha, 
+                             self.polar_data["alpha"], 
+                             self.polar_data["Cl"])
     
     def calculate_Cd(self, alpha:np.ndarray|float) -> np.ndarray|float:
         """Method to compute Cd at a given angle of attack"""
-        return np.interp(alpha,self.polar_data["alpha"],self.polar_data["Cd"])
+        if self.is_prop:
+            return np.interp(alpha, 
+                             self.polar_data["alpha"], 
+                             self.polar_data["Cd"])
+        else:
+            return np.interp(-alpha, 
+                             self.polar_data["alpha"], 
+                             self.polar_data["Cd"])
 
+    def calculate_induction(self, CT):
+        # applies Glauert correction for heavily loaded blades
+
+        CT1 = -1.816
+        CT_cr = CT1 - 2*np.sqrt(np.abs(CT1))
+        if CT < CT_cr: # heavily loaded turbine
+            a = (CT-CT1)/(4*(np.sqrt(np.abs(CT1))-1))-1
+        else: # momentum theory result
+            a = 1/2*(1-np.sqrt(CT+1))
+        return a
+    
     def calculate_residual(self, phi):
             
         # compute angles
@@ -61,43 +90,74 @@ class Annuli:
         Cl = self.calculate_Cl(alpha_deg)
         Cd = self.calculate_Cd(alpha_deg)
 
-        # rotate forces Ning 19
+        # rotate forces
         Cx = Cl*np.cos(phi)-Cd*np.sin(phi)
         Cy = Cl*np.sin(phi)+Cd*np.cos(phi)
         
-        F = tip_correction.ning_correction(self.r_R,self.r_R_H,self.B, phi)
-        RHS_1 = self.sig*Cx/(4*F*np.sin(phi)**2) #Ning (33)
-        RHS_2 = Cy*self.sig/(4*F*np.sin(phi)*np.cos(phi)) #Ning (42)
+        # calculate the tip correction
+        # F = tip_correction.ning_correction(self.r_R,self.r_R_H,self.B, phi)
+        F = tip_correction.calculate_prandtl_correction3(
+            B=self.B,
+            phi=phi,
+            r_R=self.r_R,
+            r_R_H=self.r_R_H)
         
-        a = RHS_1/(1-RHS_1) # Ning (34) must be replaced by the correction
-        aline = RHS_2/(1+RHS_2) # Ning (43)
+        # blade element momentum
+        #   tangential
+        k = Cy*self.sig/(4*F*np.sin(phi)*np.cos(phi)) # Ning (Eq 42)
+        aline = k/(1+k)
+        
+        #   axial
+        CT = Cx*self.sig*((1+a)/np.sin(phi))**2 # Ning (27)
+        a = self.calculate_induction(CT) 
 
         # compute other coefficients
-        velocity_ratio = (1+a)/((1-aline)*np.tan(phi)) #Vy/Vx derived from the expression W = Vx(1+a)/sin(phi) = Vy(1+a')/cos(phi) Ning 25 and 26
-        CT = Cx*self.sig*((1+a)/np.sin(phi))**2 # Ning (27)
-        CQ = Cy*self.sig*((1-aline)/np.cos(phi))*((1+a)/np.sin(phi))*velocity_ratio # Ning (28) 
+        Vy_Vx = self.TSR*self.r_R
+        CQ = Cy*self.sig*((1-aline)/np.cos(phi))*((1+a)/np.sin(phi))*Vy_Vx # Ning (Eq 28) 
         CP = 2*np.pi*CQ
         Ca = Cy #is this azimuthal?
 
-        residual = np.sin(phi)/(1+a) - 1/velocity_ratio*np.cos(phi)/(1-aline) # Ning (70)
+        residual = np.sin(phi)/(1+a) - 1/Vy_Vx*np.cos(phi)/(1-aline) # Ning (Eq 70)
             
-        # save converged results
-        self.alpha = alpha_deg
-        self.Cl = Cl
-        self.Cd = Cd
-        self.Cx = Cx
-        self.Cy = Cy
-        self.F  = F
-        self.CT = CT
-        self.CP = CP
-        self.Ca = Ca
-        self.CQ = CQ
-        self.a = a
-        self.aline = aline
+        # save results
+        res = dict(
+            alpha = alpha_deg,
+            Cl = Cl,
+            Cd = Cd,
+            Cx = Cx,
+            Cy = Cy,
+            F  = F,
+            CT = CT,
+            CP = CP,
+            Ca = Ca,
+            CQ = CQ,
+            a = a,
+            aline = aline,
+        )
+        self.temp_res = res
         return residual
+    
     def solve(self):
-        phi = optimize.brentq(self.calculate_residual,1E-6,np.pi/2) #First quadrant 
-        return phi
+
+        # Initial bound: 1st quadrant
+        phi0 = 1E-6
+        phi1 = np.pi/2
+        self.phi = optimize.brentq(self.calculate_residual, phi0, phi1)
+
+        # store results
+        self.alpha = self.temp_res["alpha"]
+        self.Cl = self.temp_res["Cl"]
+        self.Cd = self.temp_res["Cd"]
+        self.Cx = self.temp_res["Cx"]
+        self.Cy = self.temp_res["Cy"]
+        self.F  = self.temp_res["F"]
+        self.CT = self.temp_res["CT"]
+        self.CP = self.temp_res["CP"]
+        self.Ca = self.temp_res["Ca"]
+        self.CQ = self.temp_res["CQ"]
+        self.a = self.temp_res["a"]
+        self.aline = self.temp_res["aline"]
+
 
 if __name__ == "__main__":
     
